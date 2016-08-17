4d9a72e86511f06588d094bda129c717563b2996
angcyo <angcyo@126.com>
2016-08-17 20:22:05 +0800

    Initial Commit

---
+++ ImagePickerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImagePickerDemo
+//
+//  Created by angcyo on 16/08/17.
+//  Copyright © 2016年 angcyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
